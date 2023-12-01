db1833094b5e07fef77741851ea38a52d12d917c
Graham Hall <graham@ghall.dev>
2023-12-01 08:56:23 -0500

    Initial Commit

---
+++ sqlite-test/sqlite_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  sqlite_testApp.swift
+//  sqlite-test
+//
+//  Created by Graham Hall on 12/1/23.
+//
+
+import SwiftUI
+
+@main
+struct sqlite_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
